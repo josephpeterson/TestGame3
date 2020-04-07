8ac460fb1b3c4acaba6acaefded82e29624d9947
Joseph Peterson <josephpetersonn@gmail.com>
2020-04-07 18:59:34 -0500

    Initial Commit 3

---
+++ Source/TestGame3/TestGame3.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "TestGame3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestGame3, "TestGame3" );
+
+DEFINE_LOG_CATEGORY(LogTestGame3)
+ 
