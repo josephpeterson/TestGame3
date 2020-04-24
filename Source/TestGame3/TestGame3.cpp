1a345dde3af8ed9972b1ab4e9574f8a47af04d46
Joseph Peterson <josephpetersonn@gmail.com>
2020-04-24 13:37:28 -0500

    Remove C++

--- Source/TestGame3/TestGame3.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "TestGame3.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestGame3, "TestGame3" );
-
-DEFINE_LOG_CATEGORY(LogTestGame3)
- 
